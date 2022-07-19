9a88e9ce259da7fd96ba6e0e07ba8947d41170c1
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-07-19 20:29:49 +0500

    fixes

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -26,12 +26,36 @@ const MainLayout = ({ children }: Props) => {
       <div className="main-layout-content">
         <div className="top-section">
           <div className="container-fluid">
-            <div className="row">
-              <div className="col-3">
+            <div className="row main-row">
+              <div className="col-4">
                 <h4 className="mb-2">Update / Logs</h4>
-                <div className="text-red">Version 1.0.0</div>
+                <div className="text-green mb-1">Version 1.0.0</div>
+
+                <div className="text-start">
+                  <p className="text-light">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tempora voluptatibus labore tempore beatae!
+                  </p>
+
+                  <ul className="mt-3">
+                    <li>+ Added Discord</li>
+                    <li>- Removed Bugs</li>
+                    <li>
+                      + Added{" "}
+                      <a className="text-secondary-1" href="#0">
+                        Read More
+                      </a>
+                    </li>
+                  </ul>
+
+                  <div className="mt-2">
+                    <button className="btn">
+                      <div className="btn-text">Download Update</div>
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className="col-7">
+              <div className="col">
                 <h4 className="mb-2">License Section</h4>
 
                 <input type="text" placeholder="XXXXX-XX-XX-XXX-XXX" />
@@ -55,7 +79,20 @@ const MainLayout = ({ children }: Props) => {
           </div>
         </div>
 
-        <div className="main-section">{children}</div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-xxl-8">
+              <div className="p-4">
+                <div className="main-section">{children}</div>
+              </div>
+            </div>
+            <div className="col-xxl-4">
+              <div className="p-4">
+                <h3 className="text-center">Stats</h3>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
